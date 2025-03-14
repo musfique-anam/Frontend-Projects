caa4c41293772a3463c597a587786322f780a104
Musfique Anam <musfique2002.personal@gmail.com>
2025-03-14 17:31:12 +0600

    Updated

--- Youtube Clone/script.js
+++ Youtube Clone/script.js
@@ -1,8 +1,14 @@
 const menuButton = document.querySelectorAll(".menu-button");
+const screenOverlay = document.querySelector(".screen-overlay");
 
 //Toggle sidebar visibility when menu buttons are clicked
 menuButton.forEach(button => {
     button.addEventListener("click", () => {
         document.body.classList.toggle("sidebar-hidden");
     });
+});
+
+//Toggle sidebar visibility when screen overlay is clicked
+screenOverlay.addEventListener("click", () => {
+    document.body.classList.toggle("sidebar-hidden");
 });
